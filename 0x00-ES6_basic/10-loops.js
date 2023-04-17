e395a861336cefb148b51b77d98ccc2d4a62dc83
greenbel <obisann@gmail.com>
2023-04-17 23:34:04 +0000

    0x00-T10: addressed eslint issues.

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
+  const arrayCopy = array.slice(0);
   for (const val of array) {
-    array[idx] = appendString + val;
+    arrayCopy[idx] = appendString + val;
     idx += 1;
   }
 
-  return array;
+  return arrayCopy;
 }

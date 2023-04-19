2d4fb0ccb16116ca2cab1925236e99e3dda03d9f
greenbel <obisann@gmail.com>
2023-04-19 16:45:45 +0000

    0x01-T3: testing...

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
 export default function handleResponseFromAPI(promise) {
+  const resp = { status: 200, body: 'success' };
+
   promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
+    .then(() => resp)
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const x = uploadPhoto();
-  const y = createUser();
-  return Promise.all([x, y])
-    .then((objsList) => console.log(`${objsList[0].body} ${objsList[1].firstName} ${objsList[1].lastName}`),
-      () => console.log('Signup system offline'));
+  Promise.all([uploadPhoto(), createUser()])
+    .then((objsList) => console.log(`${objsList[0].body} ${objsList[1].firstName} ${objsList[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
 
 /*
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto, createUser])
+  Promise.all([uploadPhoto(), createUser()])
     .then((objsList) => console.log(objsList[0].body, objsList[1].firstName, objsList[1].lastName))
     .catch(() => console.log('Signup system offline'));
 }

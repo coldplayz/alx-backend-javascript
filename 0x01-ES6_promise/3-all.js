78540c16d5c09869b68a283317ab55e89aa7e627
greenbel <obisann@gmail.com>
2023-04-19 15:44:31 +0000

    0x01-T3: init

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((objsList) => console.log(objsList[0].body, objsList[1].firstName, objsList[1].lastName))
+    .catch(() => console.log('Signup system offline'));
+}
+
+/*
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto, createUser])
+    .then((objsList) => console.log(objsList[0].body, objsList[1].firstName, objsList[1].lastName))
+    .catch(() => console.log('Signup system offline'));
+}
+*/

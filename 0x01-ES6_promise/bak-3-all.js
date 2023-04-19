768047442c2474249007b5a5e6fb5e38a2cf3c4d
greenbel <obisann@gmail.com>
2023-04-19 17:05:10 +0000

    0x01-T3: testing...

--- 0x01-ES6_promise/bak-3-all.js
+++ 0x01-ES6_promise/bak-3-all.js
@@ -10,8 +10,19 @@ export default function handleProfileSignup() {
 }
 
 /*
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto, createUser])
+  Promise.all([uploadPhoto(), createUser()])
+    .then((objsList) => console.log(
+      `${objsList[0].body} ${objsList[1].firstName} ${objsList[1].lastName}`),
+      () => console.log('Signup system offline'));
+}
+*/
+
+/*
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
     .then((objsList) => console.log(objsList[0].body, objsList[1].firstName, objsList[1].lastName))
     .catch(() => console.log('Signup system offline'));
 }

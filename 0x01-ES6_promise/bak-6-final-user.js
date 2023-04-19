0beb7d290ef5f58c0c9e2c1aa01b8e3e3416a89d
greenbel <obisann@gmail.com>
2023-04-19 20:05:58 +0000

    0x01-T6: testing...

--- 0x01-ES6_promise/bak-6-final-user.js
+++ 0x01-ES6_promise/bak-6-final-user.js
@@ -1,9 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([(async () => signUpUser(firstName, lastName))(),
-    (async () => uploadPhoto(fileName))()])
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((objsList) => {
       for (const obj of objsList) {
         if (obj.status === 'rejected') {

c5f7008b96a240c80349b65931b66c8c17a2908c
greenbel <obisann@gmail.com>
2023-04-19 16:07:03 +0000

    0x01-T3: changed implementation.

--- 0x01-ES6_promise/bak-3-all.js
+++ 0x01-ES6_promise/bak-3-all.js
@@ -1,12 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const x = uploadPhoto();
-  const y = createUser();
-  return Promise.all([x, y]).then(
-    (data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
-    () => console.log('Signup system offline'),
-  );
+  Promise.all([uploadPhoto(), createUser()])
+    .then((objsList) => console.log(objsList[0].body, objsList[1].firstName, objsList[1].lastName))
+    .catch(() => console.log('Signup system offline'));
 }
 
 /*

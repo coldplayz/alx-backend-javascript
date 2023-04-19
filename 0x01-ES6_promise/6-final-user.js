509d47a2ea3969088e67d7ab4407be25af7189b6
greenbel <obisann@gmail.com>
2023-04-19 19:19:38 +0000

    0x01-T6: changed reject reason to value key

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((objsList) => objsList);
+    .then((objsList) => {
+      for (const obj of objsList) {
+        if (obj.status === 'rejected') {
+          obj.value = obj.reason;
+          delete obj.reason;
+        }
+      }
+      return objsList;
+    });
 }

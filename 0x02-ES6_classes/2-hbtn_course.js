b32c4d9a5703f421b3bb109bed215493feb43b58
greenbel <obisann@gmail.com>
2023-04-20 16:43:02 +0000

    0x02-T2: init

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+      this._name = name;
+
+      this._length = length;
+
+      this._students = students;
+  }
+
+  // Implement a getter for each [public instance] field
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Implement setters
+  set name(nm) {
+    if (nm instanceof String || typeof nm === 'string') {
+      this._name = nm;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  set length(len) {
+    if (len instanceof Number || typeof len === 'number') {
+      this._length = len;
+    } else {
+      throw TypeError('Length must be a number');
+    }
+  }
+
+  set students(stu) {
+    if (stu instanceof Array) {
+      this._students = stu;
+    } else {
+      throw TypeError('Students must be an Array');
+    }
+  }
+}

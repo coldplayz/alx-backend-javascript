8bd38cd51fe456e595f033ab4d791d0aba24cf0d
greenbel <obisann@gmail.com>
2023-04-24 22:01:12 +0000

    0x03-T8: testing...

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let str = '';
 
   for (const val of set) {
-    if (startString && val.startsWith(startString)) {
+    if (startString && val.startsWith(startString) && val.length > startString.length) {
       const startIdx = 0;
       str += (str ? '-' : '') + val.slice(startIdx + startString.length);
     }

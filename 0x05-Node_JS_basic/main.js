a90e0d2605250846404415d5c172aa2cdb93b2d2
greenbel <obisann@gmail.com>
2023-06-14 07:12:25 +0000

    0x08-T8: copied database.csv to full_server/

--- 0x05-Node_JS_basic/main.js
+++ 0x05-Node_JS_basic/main.js
@@ -1,25 +1,34 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-process.argv[2] = './blabla.csv';
-const app = require('./7-http_express');
+process.argv[2] = './database.csv';
+
+import app from './server';
+// const app = require('./full_server/server');
 
 chai.use(chaiHttp);
 chai.should();
 
-describe('More complex HTTP server using Express', () => {
-  describe('When the database is not available', () => {
-    before(() => {
-      process.argv[2] = './blabla.csv';
-    })
-    it('Returns the right error message', (done) => {
-      chai.request(app)
-        .get('/students')
-        .end((error, response) => {
-          chai.expect(response.text).to.equal(`This is the list of our students
-Cannot load the database`);
-          done();
-        });
+describe('Full HTTP server using Express', () => {
+  
+  describe('/students endpoint', () => {
+    describe('When the database is available', () => {
+      before(() => {
+        process.argv[2] = './database.csv';
+      });
+
+      it('Returns the right content', (done) => {
+        chai.request(app)
+          .get('/students')
+          .end((error, response) => {
+            chai.expect(response.statusCode).to.equal(200);
+            chai.expect(response.text).to.have.string(`This is the list of our students
+Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie
+Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy`);
+            done();
+
+          });
+      });
     });
   });
 });

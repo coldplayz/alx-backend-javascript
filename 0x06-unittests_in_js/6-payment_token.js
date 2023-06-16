6006141e2a766e0f8b5d7c4c97b912f5ed948148
greenbel <obisann@gmail.com>
2023-06-16 07:59:31 +0000

    0x06-T6: restored working implementation

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,17 +1,7 @@
 function getPaymentTokenFromAPI(success) {
-    if (success) {
-      return Promise.resolve({ result: 'Amazing result!' });
-    }
-  }
-  
-  module.exports = getPaymentTokenFromAPI;
-
-/*
-function getPaymentTokenFromAPI(success) {
   if (success === true) {
     return Promise.resolve({ data: 'Successful response from the API' });
   }
 }
 
 module.exports = getPaymentTokenFromAPI;
-*/
